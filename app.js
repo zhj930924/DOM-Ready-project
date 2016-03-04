7471dbb31e74049d22b000bea43d3b17ad2dc3df
Drew Barontini <drewbarontini@gmail.com>
2016-03-04 11:28:27 -0500

    Add the add link form and animation

--- app.js
+++ app.js
@@ -10,4 +10,10 @@ $(document).ready(function() {
      .closest('.news-item')
      .addClass('is-liked');
   });
+
+  $('.js-add-link').on('click', function(event) {
+    event.preventDefault();
+
+    $('.js-form').toggleClass('is-visible');
+  });
 });
