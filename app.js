b37f5389e1fb463b50ef108750ad241bf008e765
Drew Barontini <drewbarontini@gmail.com>
2016-03-04 11:57:18 -0500

    Add modal functionality

--- app.js
+++ app.js
@@ -3,6 +3,11 @@
 //-----------------------------------------------
 
 $(document).ready(function() {
+
+  //-----------------------------------------------
+  //   Like
+  //-----------------------------------------------
+
   $('.js-like').on('click', function(event) {
     event.preventDefault();
 
@@ -11,9 +16,29 @@ $(document).ready(function() {
      .addClass('is-liked');
   });
 
+  //-----------------------------------------------
+  //   Add Link
+  //-----------------------------------------------
+
   $('.js-add-link').on('click', function(event) {
     event.preventDefault();
 
     $('.js-form').toggleClass('is-visible');
+  });
+
+  //-----------------------------------------------
+  //   Modal
+  //-----------------------------------------------
+
+  $('.js-show-modal').on('click', function(event) {
+    event.preventDefault();
+
+    $('.js-modal').addClass('is-visible');
+    $('.js-modal-overlay').addClass('is-visible');
+  });
+
+  $('.js-modal-overlay').on('click', function(event) {
+    $('.js-modal').removeClass('is-visible');
+    $('.js-modal-overlay').removeClass('is-visible');
   });
 });
