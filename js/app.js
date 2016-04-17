13d147c3c94bc4baf13f78fb48cbf240bcf10e5b
zhj930924 <zhj930924@gmail.com>
2016-04-17 12:53:18 -0400

    Add html form

--- js/app.js
+++ js/app.js
@@ -7,38 +7,23 @@ $(document).ready(function() {
   //-----------------------------------------------
   //   Like
   //-----------------------------------------------
-
   $('.js-like').on('click', function(event) {
     event.preventDefault();
 
     $(this).text('Liked!')
-     .closest('.news-item')
-     .addClass('is-liked');
+    .closest('.news-item')
+    .addClass('is-liked');
+
   });
 
   //-----------------------------------------------
   //   Add Link
   //-----------------------------------------------
 
-  $('.js-add-link').on('click', function(event) {
-    event.preventDefault();
 
-    $('.js-form').toggleClass('is-visible');
-  });
 
   //-----------------------------------------------
   //   Modal
   //-----------------------------------------------
 
-  $('.js-show-modal').on('click', function(event) {
-    event.preventDefault();
-
-    $('.js-modal').addClass('is-visible');
-    $('.js-modal-overlay').addClass('is-visible');
-  });
-
-  $('.js-modal-overlay').on('click', function(event) {
-    $('.js-modal').removeClass('is-visible');
-    $('.js-modal-overlay').removeClass('is-visible');
-  });
 });
